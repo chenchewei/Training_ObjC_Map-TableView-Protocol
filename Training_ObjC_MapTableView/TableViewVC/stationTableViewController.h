4191ddc474a64ae642c2e60c3330d39d2c16751d
ChenCheWei <gunattack1120@gmail.com>
2020-07-10 20:56:23 +0800

    Setting up parameters

---
+++ Training_ObjC_MapTableView/TableViewVC/stationTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  stationTableViewController.h
+//  Training_ObjC_MapTableView
+//
+//  Created by mmslab-mini on 2020/7/10.
+//  Copyright © 2020 mmslab-mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface stationTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Training_ObjC_MapTableView/ViewController.m
+++ Training_ObjC_MapTableView/ViewController.m
@@ -7,8 +7,24 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
+
+//
 
 @interface ViewController ()
+
+/* 控制器上看得到的物件 */
+@property (strong,nonatomic) IBOutlet UITableView *stationTable;
+@property (strong,nonatomic) IBOutlet MKMapView *mapView;
+@property (strong,nonatomic) IBOutlet UIButton *switchingBtn;
+/* 程式會使用到的各類變數 */
+@property (strong, nonatomic) NSMutableArray<NSString *> *stationNameArr;
+@property (strong, nonatomic) NSMutableArray<NSString *> *stationAddressArr;
+@property (strong, nonatomic) NSMutableArray<NSMutableArray<NSNumber *> *> *coordinatesArr;
+@property (strong, nonatomic) NSMutableArray<MKPointAnnotation *> *annotationArr;
+
+
+
 
 @end
 
@@ -17,6 +33,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    NSArray *array = @[@"taipei",@"taipei"]; // 靜態陣列
+    NSArray *array1 = [[NSArray alloc]initWithObjects:@"taipei",@"taipei", nil];
+        
+     _stationNameArr = @[@"taipei",@"taipei"].mutableCopy;
+    _stationNameArr = [[NSMutableArray<NSString *> alloc]initWithArray:@[@"taipei",@"taipei"]];
+    _stationNameArr = [[NSMutableArray<NSString *> alloc]initWithObjects:@"taipei",@"taipei", nil];
+    
 }
 
 
